c9ef1ca98ca35350696a033f76d1227bb97cf143
Systematic01 <jonathanfrancisco999@gmail.com>
2016-07-20 22:42:42 +0800

    asdasd

--- myscript.js
+++ myscript.js
@@ -10,7 +10,6 @@ var quotes = ["Ang totoong kaibigan ay yung tatarantaduhin ka nang harapan at pu
  "When you judge another, you do not define them, you define yourself.","BOOM PANES!",
  "How people treat you is their karma; how you react is yours.",
  "WAG MONG IDADAAN SA PAGPAPATAMA SA FACEBOOK MGA PROBLEMA MO SAKIN PURO KA PARINING ITAG MO KO PARA INTENSE",
- "Whatever you give to life it gives you back. Do not hate anybody. The hatred which comes out from you will someday comeback to you. Love others and Love will comeback to you.", 
  "There are two kinds of people in the world: those who divide everybody into two kinds of people and those who don’t.", "Don't mix between my personaity and my attitude because my personality is ME and my attitude depends on YOU",
  "Whatever you give to life it gives you back. Do not hate anybody. The hatred which comes out from you will someday comeback to you. Love others and Love will comeback to you.",
  "It is during our failures that we discover our true desire for success","Sa panahon ngayon TANGHALI na lng ang tapat.","WAG MONG IDADAAN SA PAGPAPATAMA SA FACEBOOK MGA PROBLEMA MO SAKIN PURO KA PARINING ITAG MO KO PARA INTENSE",
