b0d1d0bf84f62788b17f5f1f7f85222367de27c9
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2016-07-20 10:23:58 +0800

    Boom panes
    dasdsadasd

--- myscript.js
+++ myscript.js
@@ -7,7 +7,7 @@ var quotes = ["Ang totoong kaibigan ay yung tatarantaduhin ka nang harapan at pu
  "Hindi lahat ng PATAMA ay tungkol sayo. Meron lang talagang sadyang SINASALO MO!",
  "Lies hurt people; imagination makes life more fun.", 
  "Naniniwala ako sa kasabihan ng ang LOVE ay parang sigarilyo, bat pa naimbento kung sakit naman ang dulot!",
- "When you judge another, you do not define them, you define yourself.",
+ "When you judge another, you do not define them, you define yourself.","BOOM PANES!",
  "How people treat you is their karma; how you react is yours.",
  "Whatever you give to life it gives you back. Do not hate anybody. The hatred which comes out from you will someday comeback to you. Love others and Love will comeback to you.", 
  "There are two kinds of people in the world: those who divide everybody into two kinds of people and those who don’t.", "Don't mix between my personaity and my attitude because my personality is ME and my attitude depends on YOU",
