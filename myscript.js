40c5a2639f94def7fe07b0d5df92a629fe4b11b5
Systematic01 <jonathanfrancisco999@gmail.com>
2016-07-20 22:44:31 +0800

    dasd

--- myscript.js
+++ myscript.js
@@ -12,7 +12,7 @@ var quotes = ["Ang totoong kaibigan ay yung tatarantaduhin ka nang harapan at pu
  "WAG MONG IDADAAN SA PAGPAPATAMA SA FACEBOOK MGA PROBLEMA MO SAKIN PURO KA PARINING ITAG MO KO PARA INTENSE",
  "There are two kinds of people in the world: those who divide everybody into two kinds of people and those who don’t.", "Don't mix between my personaity and my attitude because my personality is ME and my attitude depends on YOU",
  "Whatever you give to life it gives you back. Do not hate anybody. The hatred which comes out from you will someday comeback to you. Love others and Love will comeback to you.",
- "It is during our failures that we discover our true desire for success","Sa panahon ngayon TANGHALI na lng ang tapat.","WAG MONG IDADAAN SA PAGPAPATAMA SA FACEBOOK MGA PROBLEMA MO SAKIN PURO KA PARINING ITAG MO KO PARA INTENSE",
+ "It is during our failures that we discover our true desire for success","Sa panahon ngayon TANGHALI na lng ang tapat.","WAG MONG IDADAAN SA PAGPAPATAMA SA FACEBOOK MGA PROBLEMA MO SAKIN PURO KA PARINIG ITAG MO KO PARA INTENSE",
   "DIBA? PAK GANERN!","DBA? DBA? DBA?", "KALA MO OKAY NA, HINDI PA PALA", "PAK! DBA? PAK NA PAK!","Beauty is an enormous, unmerited gift given randomly, stupidly","A dream all a dream that ends in nothing and leaves the sleeper where he lay down but I wish you to know that you inspired it."];
 
 $(".pagpindotan").click(function(){
